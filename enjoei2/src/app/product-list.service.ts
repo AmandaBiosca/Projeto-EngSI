8b6be0a50cb82bd4955d757f808f38ef4bc136e7
PBeani <bunnyzones25@gmail.com>
2018-12-02 21:02:51 -0200

    adicionando barra de busca

--- enjoei2/src/app/product-list.service.ts
+++ enjoei2/src/app/product-list.service.ts
@@ -16,14 +16,20 @@ export class ProductListService {
     headers: new HttpHeaders({
       'Content-Type': 'application/json'
     })
-  };
+  }
 
-  popularURL = 'http://ec2-54-185-32-212.us-west-2.compute.amazonaws.com:8080/requisitar/maispop';
+  popularURL = 'http://ec2-54-185-32-212.us-west-2.compute.amazonaws.com:8080/requisitar/maispop/24';
+  userUrl = 'http://ec2-54-185-32-212.us-west-2.compute.amazonaws.com:8080/requisitar/cliente/';
+  searchUrl = 'http://ec2-54-185-32-212.us-west-2.compute.amazonaws.com:8080/requisitar/busca/$$search/$$category/$$id/$$min/$$max/$$order'
 
   constructor(private http: HttpClient) { }
 
   getPopularProducts(): Observable<any> {
-    return this.http.get(this.popularURL, this.httpOptions);
+    return this.http.get(this.popularURL, {responseType: 'text'});
+  }
+
+  getUserProducts(id: number): Observable<any> {
+    return this.http.get(this.userUrl + id, this.httpOptions);
   }
 
   getProducts(): Product[] {
@@ -32,5 +38,25 @@ export class ProductListService {
 
   getProductsId(): Product[] {
     return PRODUCTS1;
+  }
+
+  getFilterProducts(params: any):  Observable<any> {
+    let customUrl = this.searchUrl;
+    
+    params.search = params.search.replace('$$', '$');
+
+    customUrl = customUrl.replace('$$search', params.search || 'None');  
+    customUrl = customUrl.replace('$$category', params.category || 'None');  
+    customUrl = customUrl.replace('$$id', params.id || 'None');  
+    customUrl = customUrl.replace('$$min', params.min || 'None');  
+    customUrl = customUrl.replace('$$max', params.max || 'None');  
+    customUrl = customUrl.replace('$$order', params.order || 'None'); 
+
+    return this.http.get(customUrl, {responseType: 'text'});
+  }
+
+  getResults(idList: number[]): Observable<any> {
+    // mudar para outro serviço
+    return this.http.get(this.popularURL, {responseType: 'text'});
   }
 }
--- enjoei2/src/app/product-list/product-list.component.ts
+++ enjoei2/src/app/product-list/product-list.component.ts
@@ -1,27 +1,170 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Product } from '../objects/product';
 import { ProductListService } from '../product-list.service';
+import { ToastrService } from 'ngx-toastr';
+import { PRODUCTS } from '../mock-data/products';
+import { CommunicationService } from '../communication.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
 
   productList: Product[];
+  id: number;
+  requestLine: number[];
+  hasMore: boolean = false;
+  loading: boolean = false;
+  title: string = 'Populares';
+  hardLoad: boolean = true;
+  subscription: any;
+  subscription2: any;
+  subscription3: any;
+  search: string = '';
 
-  constructor(private productListService: ProductListService) { }
+  constructor(private productListService: ProductListService,
+    private toastr: ToastrService, private communication: CommunicationService,
+    private route: ActivatedRoute) { }
 
-  getProducts(): void {
-    // this.productListService.getPopularProducts().subscribe(
-    //   data => {}, error => {}
-    // );
 
-    this.productList = this.productListService.getProducts();
+  resetProductList(newIds: number[]) {
+    this.productList = [];
+    //this.requestLine = newIds;
+    this.requestLine = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+
+    this.getPage();
+  }
+
+  getPage() {
+    if (this.requestLine.length) {
+      this.loading = true;
+      let nextRequest = this.requestLine.splice(0, 8);
+      this.productListService.getResults(nextRequest).subscribe(
+        data => {
+          //mock
+          data = PRODUCTS;
+          for (let i = 0; i < data.length; i++) {
+            this.productList.push(data[i]);
+          }
+
+          if (this.requestLine.length > 0) {
+            this.hasMore = true;
+          } else {
+            this.hasMore = false;
+          }
+          this.hardLoad = false;
+          this.loading = false;
+        },
+        error => {
+          this.toastr.error('Não foi possível obter os produtos solicitados', 'Algo deu errado!');
+          console.log(error);
+          this.loading = false;
+          this.hardLoad = false;
+        }
+      )
+    } else {
+      this.hardLoad = false;
+    }
+  }
+
+  getUserProducts(): void {
+    this.search = null;
+    this.hardLoad = true;
+    if (this.id) {
+      this.title = "Especialmente para você"
+      this.productListService.getUserProducts(this.id).subscribe(
+        data => {
+          this.resetProductList([1, 2]);
+        },
+        error => {
+          this.toastr.error('Não foi possível obter os produtos para o usuário', 'Algo deu errado!');
+          console.log(error);
+          this.hardLoad = false;
+        }
+      )
+    } else {
+      this.title = "Populares"
+      this.productListService.getPopularProducts().subscribe(
+        data => {
+          this.resetProductList([1, 2]);
+        },
+        error => {
+          this.toastr.error('Não foi possível obter os produtos populares', 'Algo deu errado!');
+          console.log(error);
+          this.hardLoad = false;
+        }
+      )
+    }
+  }
+
+  searchAction(search: string) {
+
+    var params: any = {
+      search: search
+    }
+
+    if (this.id >= 0) {
+      params.id = this.id;
+    }
+
+    this.productListService.getFilterProducts(params).subscribe(
+      data => {
+        this.resetProductList([1, 2]);
+      },
+      error => {
+        this.toastr.error('Não foi possível realziar a busca', 'Algo deu errado!');
+        console.log(error);
+        this.hardLoad = false;
+      }
+    )
+  }
+
+  initialize() {
+    let id: any = window.localStorage.getItem('enjoojeiUserID');
+    id = parseInt(id);
+    if (id >= 0) {
+      this.id = parseInt(id);
+    } else {
+      this.id = null;
+    }
+
+    this.getUserProducts();
   }
 
   ngOnInit() {
-    this.getProducts();
+    this.initialize();
+
+    this.subscription2 = this.communication.triggerAccess.subscribe(
+      data => {
+        this.initialize();
+      }
+    )
+
+    this.subscription3 = this.communication.triggerLogout.subscribe(
+      data => {
+        this.initialize();
+      }
+    )
+
+    this.subscription = this.communication.triggerSearch.subscribe(
+      string => {
+        this.hardLoad = true;
+        this.search = string;
+        if (string.trim() !== '') {
+          this.searchAction(string);
+        } else {
+          this.getUserProducts();
+        }
+      }
+    )
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+    this.subscription2.unsubscribe();
+    this.subscription3.unsubscribe();
   }
 }
