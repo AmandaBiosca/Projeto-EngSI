166c5c0be3ed69e60d7b8689bc58ea1d0b305366
PBeani <bunnyzones25@gmail.com>
2018-12-03 22:41:45 -0200

    adicionando botão de salvar e criando objeto de evnllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll

--- enjoei2/src/app/product-register/product-register.component.ts
+++ enjoei2/src/app/product-register/product-register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductRegister } from '../objects/productRegister';
 
 @Component({
   selector: 'app-product-register',
@@ -7,6 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductRegisterComponent implements OnInit {
 
+  form: ProductRegister = {
+    a_venda: true,
+    categoria: '',
+    condicao: '',
+    desconto: 0, 
+    descricao: '', 
+    foto: '', 
+    frete: 0,
+    id_comprador: null, 
+    id_vendedor: 0, 
+    marca: '', 
+    nome: '',
+    preco: 0,
+    qtde_curtidas: 0,
+    tags: '',
+    tamanho: 'medio'
+  }
   constructor() { }
 
   ngOnInit() {
